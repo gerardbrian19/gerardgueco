e736b79b38645e015900e64bcaf4663346f445a8
GGueco <43228146+gerardbrian19@users.noreply.github.com>
2020-06-09 14:10:15 +0800

    added initalprops

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,42 @@
+import App from 'next/app'
+import React from 'react'
+import { PageTransition } from 'next-page-transitions'
 import 'semantic-ui-css/semantic.min.css'
 import '../public/static/styles/main.css'
 import '../public/static/styles/animate.min.css'
 
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    const res = await import('../public/static/data/projects.json')
+    return { pageProps: res }
   }
-  
+  componentDidMount(e){
+    console.log(this.props);
+  }
+  render() {
+    const { Component, pageProps, router } = this.props
+    return (
+      <div>
+        <PageTransition timeout={300} classNames="page-transition">
+          <Component {...pageProps} key={router.route} />
+        </PageTransition>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transition: opacity 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
